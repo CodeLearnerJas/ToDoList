1397fbfe8011ad5ef9d5318bdefb87711c0d7eb6
CodeLearnerJas <yangtianjin2013@gmail.com>
2024-09-18 14:33:06 +1000

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by GuitarLearnerJas on 18/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
